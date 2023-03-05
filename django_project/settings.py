6bd3e478996d466eca2fd2fea25ea568fa4e6f6a
iamrizwan077 <77151292+iamrizwan077@users.noreply.github.com>
2023-03-05 17:21:48 +0500

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
-CSRF_TRUSTED_ORIGINS = ['https://d2b5-119-160-35-62.ap.ngrok.io']
 
 # Application definition
 
@@ -75,24 +74,6 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': #'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': #'django.db.backends.postgresql',
-#        'NAME': 'edsukixe',
-#        'USER': 'edsukixe',
-#        'PASSWORD': #'fAli3e1Blr0getrq5dDOSfNQ2B_Rmjuz',
-#        'HOST': #'castor.db.elephantsql.com',
-#        'PORT': 5432
-#    }
-#}
 
 DATABASES = {
     'default': {
